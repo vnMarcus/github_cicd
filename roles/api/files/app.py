90f29b8c65a0250cdaa70f54dc53ee1e0034c303
vnMarcus <ndvinhcn@gmail.com>
2023-05-17 23:03:49 +0700

    test

--- roles/api/files/app.py
+++ roles/api/files/app.py
@@ -80,4 +80,4 @@ def update_student(id):
     return jsonify({'message': 'Student updated successfully'}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9090, debug=True)
+    app.run(host='0.0.0.0', port=9090)
